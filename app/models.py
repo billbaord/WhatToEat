c7336d3865eebd96057f1d61535ff89d3aaa81b6
billbaord <165834678+billbaord@users.noreply.github.com>
2025-04-16 13:20:44 +0200

    update models.py
    (Alina - worked on 1 laptop)
    
    Implement Recipe and Category Class
    
    Using Flask-login, created function to retrieve user from database using ID stored in the session.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,3 +18,21 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    instructions = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    recipes = db.relationship('Recipe', backref='category', lazy='dynamic')
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
