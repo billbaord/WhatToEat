e33ca82d82d5f95de2c3a46e6162389aa28dae3d
billbaord <165834678+billbaord@users.noreply.github.com>
2025-04-22 17:45:34 +0200

    Update Forms.py
    Creating the Registeration and Recipe classes for registeration and recipe functions.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,38 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User, Category
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validator=[DataRequired()])
     password = PasswordField('Password', validators=DataRequired())
     remember_me_boolean = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+class RegisterationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_repeat = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username. ')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address. ')
+        
+class RecipeForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
+    description = TextAreaField('Description')
+    instructions = TextAreaField('Instructions', validators=[DataRequired()])
+    category = SelectField('Category', coerce=int)
+    submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.category.choices = [(c.id, c.name) for c in Category.query.order_by(Category.name)] 
